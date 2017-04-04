f1587ca40c3e86f451bb596e8630b89327f91021
Josh Harwood <joshuamharwood@gmail.com>
2017-04-04 23:23:44 +0100

    added overview table of devices

--- src/app/dashboard/overview/overview.component.ts
+++ src/app/dashboard/overview/overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DeviceService } from '../../device.service';
 
 @Component({
   selector: 'app-overview',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./overview.component.scss']
 })
 export class OverviewComponent implements OnInit {
+  devices : any[]
 
-  constructor() { }
+  constructor(private deviceService : DeviceService) { }
 
   ngOnInit() {
+    this.deviceService.devices.subscribe(
+      devices => {
+        this.devices = devices;
+      }
+    );
   }
 
 }
