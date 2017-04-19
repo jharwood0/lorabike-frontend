254e9e89bd578cb12b6f9048ca30650311292dfb
Josh Harwood <joshuamharwood@gmail.com>
2017-04-19 10:51:10 +0100

    form for device reg

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from './auth.service';
-
+import { JwtHelper } from 'angular2-jwt';
 @Injectable()
 export class AuthGuard implements CanActivate {
+  jwtHelper: JwtHelper = new JwtHelper();
   constructor(private authService: AuthService, private router: Router) {}
   canActivate() {
     if(this.authService.isLoggedIn()){
-      return true;
+      if(this.jwtHelper.isTokenExpired(this.authService.getToken())){
+        this.authService.logout();
+        this.router.navigate(['/login']);
+        return false;
+      }else{
+        return true;
+      }
     }else{
       this.router.navigate(['/login']);
       return false;
